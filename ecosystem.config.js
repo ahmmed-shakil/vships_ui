3ec410d0037225176f999fe28da012c1660e587a
ahmmed-shakil <shakilahmmed252@gmail.com>
2026-03-03 11:26:37 +0600

    fix: use npm start in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'perfomax-client',
-      script: 'node_modules/.bin/next',
-      args: 'start -p 3000',
+      script: 'npm',
+      args: 'start',
       cwd: './',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
+        PORT: 3000,
       },
     },
   ],
